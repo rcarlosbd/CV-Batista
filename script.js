243ddcd3345ff381d33cba70183545d250791e0e
Roberto Batista <127065490+rcarlosbd@users.noreply.github.com>
2023-07-15 03:35:27 -0300

    commit versión final
    Actualización y optimización del código

--- script.js
+++ script.js
@@ -1,64 +1,7 @@
-// /* ********** Menu ********** */
-// ((d) => {
-//   const $btnMenu = d.querySelector(".menu-btn"),
-//     $menu = d.querySelector(".menu");
-
-//   $btnMenu.addEventListener("click", (e) => {
-//     $btnMenu.firstElementChild.classList.toggle("none");
-//     $btnMenu.lastElementChild.classList.toggle("none");
-//     $menu.classList.toggle("is-active");
-//   });
-
-//   d.addEventListener("click", (e) => {
-//     if (!e.target.matches(".menu a")) return false;
-
-//     $btnMenu.firstElementChild.classList.remove("none");
-//     $btnMenu.lastElementChild.classList.add("none");
-//     $menu.classList.remove("is-active");
-//   });
-// })(document);
-
-// /* ********** ContactForm ********** */
-// ((d) => {
-//   const $form = d.querySelector(".contact-form"),
-//     $spiner = d.querySelector(".contact-form-spiner"),
-//     $response = d.querySelector(".contact-form-response");
-
-//   $form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     $spiner.classList.remove("none");
-//     fetch("https://formsubmit.co/ajax/rcbatista1987@gmail.com", {
-//       method: "POST",
-//       body: new FormData(e.target),
-//     })
-//       .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-//       .then((json) => {
-//         console.log(json);
-//         location.hash = "#gracias";
-//         $form.reset();
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         let message =
-//           err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
-//         $response.querySelector(
-//           "h3"
-//         ).innerHTML = `Error ${err.status}: ${message}`;
-//       })
-//       .finally(() => {
-//         $spiner.classList.add("none");
-//         setTimeout(() => {
-//           location.hash = "#close";
-//         }, 3000);
-//       });
-//   });
-// })(document);
-
-/*****Menu*****/
-
 ((d) => {
   const $btnMenu = d.querySelector(".menu-btn"),
-    $menu = d.querySelector(".menu");
+    $menu = d.querySelector(".menu"),
+    $logoLink = d.querySelector(".logo a");
 
   $btnMenu.addEventListener("click", (e) => {
     $btnMenu.firstElementChild.classList.toggle("none");
@@ -68,10 +11,17 @@
 
   d.addEventListener("click", (e) => {
     if (!e.target.matches(".menu a")) return false;
-
     $btnMenu.firstElementChild.classList.remove("none");
     $btnMenu.lastElementChild.classList.add("none");
     $menu.classList.remove("is-active");
+  });
+
+  $logoLink.addEventListener("click", (e) => {
+    if ($menu.classList.contains("is-active")) {
+      $btnMenu.firstElementChild.classList.remove("none");
+      $btnMenu.lastElementChild.classList.add("none");
+      $menu.classList.remove("is-active");
+    }
   });
 })(document);
 
